6b17bcd9c29d60f173c11190e407b19ffd654c69
mikemeza01 <mmeza90794@ufide.ac.cr>
2023-07-05 23:27:47 -0600

    Ajustes al proyecto con plantilla
    Se agrega plantilla, se hacen algunos ajustes, se crea y agrega logo, proyecto con plantilla funcionando.

---
+++ ControlEmpleados/ControlEmpleados/Models/ControlEmpleadosContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControlEmpleados.Models
+{
+    public class ControlEmpleadosContext : DbContext
+    {
+        public ControlEmpleadosContext(DbContextOptions<ControlEmpleadosContext> opciones)
+        : base(opciones)
+        {
+        }
+        //Entidades o las tables de la DB
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ ControlEmpleados/ControlEmpleados/Models/Usuarios.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlEmpleados.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int ID_USUARIO { get; set; }
+
+        public string? NOMBRE { get; set; }
+
+        public string? APELLIDOS { get; set; }
+
+        public int CEDULA { get; set; }
+
+        public string? CORREO { get; set; }
+
+        public int ID_HORARIO { get; set; }
+
+        public int PAGO_POR_HORA { get; set; }
+
+        public int ID_PEDIODO { get; set; }
+
+        public int ID_ROL { get; set; }
+
+        public int VACACIONES_DISPONIBLES { get; set; }
+
+        public int ID_ESTADO { get; set; }
+    }
+}
