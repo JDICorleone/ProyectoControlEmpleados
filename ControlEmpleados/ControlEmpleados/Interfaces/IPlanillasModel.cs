c37ba912b6814d0967c6858e9604005a1c38fcad
JDICorleone <88350442+JDICorleone@users.noreply.github.com>
2023-08-05 13:53:19 -0600

    Revert "Cambios en Planilla"
    This reverts commit 3971cf4860c86cd583b1fd2263aeb72d8110ee46.

--- ControlEmpleados/ControlEmpleados/Interfaces/IPlanillasModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ControlEmpleados.Entities;
-
-namespace ControlEmpleados.Interfaces
-{
-    public interface IPlanillasModel
-    {
-        public List<Planilla>? ConsultarPlanillas();
-    }
-}
--- ControlEmpleados/ControlEmpleados/Program.cs
+++ ControlEmpleados/ControlEmpleados/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddSession();
 builder.Services.AddScoped<IUsuariosModel, UsuariosModel>();
 builder.Services.AddScoped<IEmpleadosModel, EmpleadosModel>();
-builder.Services.AddScoped<IPlanillasModel, PlanillasModel>();
 
 
 var app = builder.Build();
