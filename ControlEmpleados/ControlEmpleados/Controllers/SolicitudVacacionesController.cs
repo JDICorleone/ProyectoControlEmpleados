873009b6a4772340cce25a644fdb324b39d10f0c
mikemeza01 <mmeza90794@ufide.ac.cr>
2023-08-20 19:32:44 -0600

    Revert "Capa presentación entrega de avance"
    This reverts commit aafb47f9f996f9483c295eae7555a22a1b7275b5.

--- ControlEmpleados/ControlEmpleados/Controllers/SolicitudVacacionesController.cs
+++ ControlEmpleados/ControlEmpleados/Controllers/SolicitudVacacionesController.cs
@@ -1,5 +1,4 @@
-﻿using ControlEmpleados.Entities;
-using ControlEmpleados.Filters;
+﻿using ControlEmpleados.Filters;
 using ControlEmpleados.Interfaces;
 using ControlEmpleados.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,12 +8,10 @@ namespace ControlEmpleados.Controllers
     public class SolicitudVacacionesController : Controller
     {
         private readonly ISolicitudVacacionesModel _solicitudVacacionesModel;
-        private readonly IEmpleadosModel _empleadosModel;
 
-        public SolicitudVacacionesController(ISolicitudVacacionesModel solicitudVacacionesModel, IEmpleadosModel empleadosModel)
+        public SolicitudVacacionesController(ISolicitudVacacionesModel solicitudVacacionesModel)
         {
             _solicitudVacacionesModel = solicitudVacacionesModel;
-            _empleadosModel = empleadosModel;
         }
 
         [FiltroValidarAdmin]
@@ -26,47 +23,6 @@ namespace ControlEmpleados.Controllers
                 return View(datos);
             }
             catch (Exception)
-            {
-                return View("Error");
-            }
-        }
-
-        [HttpGet]
-        public IActionResult AgregarSolicitud() {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AgregarSolicitud(Solicitud_Vacaciones entidad)
-        {
-            try
-            {
-                int idUsuario = int.Parse(HttpContext.Session.GetString("ID").ToString());
-
-                var empleado = _empleadosModel.ConsultarEmpleados2().FirstOrDefault(x => x.ID_USUARIO == idUsuario);
-
-                entidad.ID_EMPLEADO = empleado.ID_EMPLEADO;
-
-                int cantidadDias = _solicitudVacacionesModel.cantidadVacaciones(entidad.FECHA_INICIO,entidad.FECHA_FINAL);
-
-                entidad.CANTIDAD_DIAS = cantidadDias;
-                entidad.ESTADO = "Pendiente";
-                entidad.ID_SOLICITUD_VACACIONES = 0;
-
-                var resultado = _solicitudVacacionesModel.AgregarSolicitud(entidad);
-
-                if (resultado > 0)
-                {
-                    ViewBag.mensaje = "OK";
-                    return View(entidad);
-                }
-                else
-                {
-                    ViewBag.mensaje = "ERROR";
-                    return View();
-                }
-            }
-            catch (Exception ex)
             {
                 return View("Error");
             }
--- ControlEmpleados/ControlEmpleados/Interfaces/ISolicitudVacacionesModel.cs
+++ ControlEmpleados/ControlEmpleados/Interfaces/ISolicitudVacacionesModel.cs
@@ -1,14 +1,9 @@
 ﻿using ControlEmpleados.Consults;
-using ControlEmpleados.Entities;
 
 namespace ControlEmpleados.Interfaces
 {
     public interface ISolicitudVacacionesModel
     {
         public List<ConsultarSolicitudVacaciones>? ConsultarSolicitudes();
-
-        public int AgregarSolicitud(Solicitud_Vacaciones entidad);
-
-        public int cantidadVacaciones(DateTime FECHA_INICIO, DateTime FECHA_FINAL);
     }
 }
