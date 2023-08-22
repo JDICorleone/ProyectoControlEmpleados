bf831037b7afaec0481a8b13fde1970b4b66b6e1
mikemeza01 <mmeza90794@ufide.ac.cr>
2023-08-21 19:49:05 -0600

    Agregar solicitud de vacaciones funcionando.

--- ControlEmpleados/ControlEmpleados/Controllers/SolicitudVacacionesController.cs
+++ ControlEmpleados/ControlEmpleados/Controllers/SolicitudVacacionesController.cs
@@ -1,4 +1,5 @@
-﻿using ControlEmpleados.Filters;
+﻿using ControlEmpleados.Entities;
+using ControlEmpleados.Filters;
 using ControlEmpleados.Interfaces;
 using ControlEmpleados.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +9,12 @@ namespace ControlEmpleados.Controllers
     public class SolicitudVacacionesController : Controller
     {
         private readonly ISolicitudVacacionesModel _solicitudVacacionesModel;
+        private readonly IEmpleadosModel _empleadosModel;
 
-        public SolicitudVacacionesController(ISolicitudVacacionesModel solicitudVacacionesModel)
+        public SolicitudVacacionesController(ISolicitudVacacionesModel solicitudVacacionesModel, IEmpleadosModel empleadosModel)
         {
             _solicitudVacacionesModel = solicitudVacacionesModel;
+            _empleadosModel = empleadosModel;
         }
 
         [FiltroValidarAdmin]
@@ -23,6 +26,47 @@ namespace ControlEmpleados.Controllers
                 return View(datos);
             }
             catch (Exception)
+            {
+                return View("Error");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult AgregarSolicitud() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AgregarSolicitud(Solicitud_Vacaciones entidad)
+        {
+            try
+            {
+                int idUsuario = int.Parse(HttpContext.Session.GetString("ID").ToString());
+
+                var empleado = _empleadosModel.ConsultarEmpleados2().FirstOrDefault(x => x.ID_USUARIO == idUsuario);
+
+                entidad.ID_EMPLEADO = empleado.ID_EMPLEADO;
+
+                int cantidadDias = _solicitudVacacionesModel.cantidadVacaciones(entidad.FECHA_INICIO,entidad.FECHA_FINAL);
+
+                entidad.CANTIDAD_DIAS = cantidadDias;
+                entidad.ESTADO = "Pendiente";
+                entidad.ID_SOLICITUD_VACACIONES = 0;
+
+                var resultado = _solicitudVacacionesModel.AgregarSolicitud(entidad);
+
+                if (resultado > 0)
+                {
+                    ViewBag.mensaje = "OK";
+                    return View(entidad);
+                }
+                else
+                {
+                    ViewBag.mensaje = "ERROR";
+                    return View();
+                }
+            }
+            catch (Exception ex)
             {
                 return View("Error");
             }
